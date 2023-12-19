9f542e00eb6f5208448712c6e20f930f11cf426d
alan peña <alanert04@gmail.com>
2023-12-19 17:45:23 -0600

    unsolved

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,5 +1,7 @@
 class Instrument < ApplicationRecord
   validates :name, :price, :condition, :category, presence: true
   belongs_to :user
+  has_many :bookings, dependent: :destroy
+  has_many :users, through: :bookings
   has_one_attached :photo
 end
--- db/migrate/20231219225356_create_bookings.rb
+++ db/migrate/20231219225356_create_bookings.rb
@@ -4,6 +4,8 @@ class CreateBookings < ActiveRecord::Migration[7.1]
       t.date :pick_up_date
       t.date :drop_off_date
       t.integer :final_price
+      t.references :user, null: false, foreign_key: true
+      t.references :instrument, null: false, foreign_key: true
 
       t.timestamps
     end
