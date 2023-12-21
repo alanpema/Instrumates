d7894ae63b5fc63ce41793796e7a9d79d0b1db42
alan peña <alanert04@gmail.com>
2023-12-21 16:46:40 -0600

    calendar with range, final price added/and to cards

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -12,9 +12,21 @@ class Instrument < ApplicationRecord
 
   def not_available
     bookings.map do |booking|
-      [booking.pick_up_date.strftime("%Y-%m-%d"), booking.drop_off_date.strftime("%Y-%m-%d")]
-
-        #Instrument.find(8).bookings.map(&:pick_up_date)[0].strftime("%Y-%m-%d")
+      get_dates_in_range(booking.pick_up_date.strftime('%Y-%m-%d'), booking.drop_off_date.strftime('%Y-%m-%d'))
     end.flatten.join(" ")
   end
+
+  def get_dates_in_range(start_date, end_date)
+    require 'date'
+    start_date = Date.parse(start_date)
+    end_date = Date.parse(end_date)
+    dates = []
+
+    start_date.upto(end_date) do |date|
+      dates << date.strftime('%Y-%m-%d')
+    end
+
+    dates
+  end
+
 end
