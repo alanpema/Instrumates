3ac076ed6aac8c801578d2c2ec1d1b6b25439af5
alan peña <alanert04@gmail.com>
2023-12-21 14:00:22 -0600

    added calendar to book form

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -3,4 +3,12 @@ class Instrument < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
+
+  def not_available
+    bookings.map do |booking|
+      [booking.pick_up_date.strftime("%Y-%m-%d"), booking.drop_off_date.strftime("%Y-%m-%d")]
+
+        #Instrument.find(8).bookings.map(&:pick_up_date)[0].strftime("%Y-%m-%d")
+    end.flatten.join(" ")
+  end
 end
